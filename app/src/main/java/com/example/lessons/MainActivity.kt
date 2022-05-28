9ffb577fa048ccd5e8b64fb34384f75b4e5971c5
anbv731 <anbv731@yandex.ru>
2022-05-29 00:28:06 +0400

    part 2

--- app/src/main/java/com/example/lessons/MainActivity.kt
+++ app/src/main/java/com/example/lessons/MainActivity.kt
@@ -1,11 +1,47 @@
 package com.example.lessons
 
+import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.inputmethod.EditorInfo
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.Toast
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 
 class MainActivity : AppCompatActivity() {
+   private lateinit var editText:EditText
+   private lateinit var imageView:ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        editText = findViewById(R.id.editTextId)
+        imageView = findViewById(R.id.image)
+           editText .onSubmit { setImage(editText.text.toString()) }
+    }
+    private fun EditText.onSubmit(func: () -> Unit) {
+        setOnEditorActionListener { _, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                func()
+            }
+            true
+
+        }
+    }
+    private fun setImage(path:String){
+
+        Glide.with(this).load(path).listener(object : RequestListener<Drawable> {
+            override fun onLoadFailed(p0: GlideException?, p1: Any?, p2: Target<Drawable>?, p3: Boolean): Boolean {
+                Toast.makeText(applicationContext,"Image load failed",Toast.LENGTH_SHORT).show()
+                return false
+            }
+            override fun onResourceReady(p0: Drawable?, p1: Any?, p2: Target<Drawable>?, p3: DataSource?, p4: Boolean): Boolean {
+                return false
+            }
+        }).into(imageView)
     }
 }

1f58f61788df2bb312cc10ff9df92585d8a03eb4
anbv731 <anbv731@yandex.ru>
2022-06-01 12:51:20 +0400

    complete

--- app/src/main/java/com/example/lessons/MainActivity.kt
+++ app/src/main/java/com/example/lessons/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.example.lessons
 
+import android.graphics.Canvas
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.View
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
 }
